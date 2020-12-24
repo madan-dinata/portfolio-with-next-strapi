3b9dbe50f14c3abbd1ab7bb3ad348b389e520091
madan-dinata <madanmr48@gmail.com>
2020-12-24 14:09:25 +0700

    initial commit

---
+++ portfolio-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fd731812fed3bd4788e53dc534426876'),
+    },
+  },
+});
---
+++ portfolio-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74cb1857-d59a-4e4a-846c-1df696c36d17'
+};
